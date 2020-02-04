bc630fb6cc56e329b52c4d09de0af0f3eff587f6
Shawn <shawntimmel@gmail.com>
2020-02-04 22:44:10 +0800

    sumnum

--- src/sumnum.java
+++ src/sumnum.java
@@ -1,3 +1,11 @@
 public class sumnum {
+    int[] nums = new int[]{2, 7, 11, 15};
+    int target = 9;
+    public int[] twoSum(int[] nums, int target) {
+        int i = 0;
+        for (int n : nums) {
 
+        }
+        return nums;
+    }
 }
