97eec42bd9432a09b8f2a468256f493df1f49b4f
Shawn <shawntimmel@gmail.com>
2020-02-05 23:34:45 +0800

    sumnum1 hashmap

--- src/sumnum.java
+++ src/sumnum.java
@@ -1,11 +1,44 @@
-public class sumnum {
-    int[] nums = new int[]{2, 7, 11, 15};
-    int target = 9;
-    public int[] twoSum(int[] nums, int target) {
-        int i = 0;
-        for (int n : nums) {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+public class sumnum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int n = 0; n < nums.length; n++) {
+            for (int m = nums.length - 1; m > n; m--) {
+                if (target == nums[n] + nums[m]){
+                    return new int[]{n, m};
+                }
+            }
         }
-        return nums;
+        return null;
+    }
+
+    public static int[] twoSum1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+        for (int j = 0; j < nums.length; j++){
+            int find = target - nums[j];
+            if (map.containsKey(find) && (find != nums[j])) {
+                return new int[]{j, map.get(find)};
+            }
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 18;
+        int target1 = 18;
+        int[] position = twoSum(nums, target);
+        int[] position1 = twoSum1(nums, target1);
+        int[] position2 = twoSum(nums, target);
+        System.out.println(Arrays.toString(position));
+        System.out.println(Arrays.toString(position1));
+        System.out.println(Arrays.toString(position2));
     }
 }
