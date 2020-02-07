804adcbf5663f33955e02676b8b44afa6149c033
Shawn <shawntimmel@gmail.com>
2020-02-07 22:25:17 +0800

    add linklist

---
+++ src/AddLinklist.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class ListNode{
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+}
+
+public class AddLinklist {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return l1;
+    }
+}
