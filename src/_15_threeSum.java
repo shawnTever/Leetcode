cdac1a5e6123ca26bff8c769387702e42832b63a
Shawn <shawntimmel@gmail.com>
2020-12-17 18:04:29 +0800

    _15_threeSum.java

--- src/_15_threeSum.java
+++ src/_15_threeSum.java
@@ -12,32 +12,57 @@ public class _15_threeSum {
         return null;
     }
 
+//    public static List<List<Integer>> threeSum(int[] nums) {
+//        Arrays.sort(nums);
+//        List<List<Integer>> result = new ArrayList<>();
+//        int[] newNums = new int[nums.length-1];
+//        for (int i = 0; i < nums.length; i++) {
+//            if(i > 0 && nums[i] == nums[i-1]) continue;
+//            for (int j = 0; j < nums.length - 1; j++) {
+//                if (j < i)
+//                    newNums[j] = nums[j];
+//                else
+//                    newNums[j] = nums[j + 1];
+//            }
+//            if (twoSum(newNums, -nums[i]) != null) {
+//                List<Integer> threeSum = new ArrayList<>();
+//                threeSum.add(nums[i]);
+//                for (int n : twoSum(newNums, -nums[i]))
+//                    threeSum.add(n);
+//                Collections.sort(threeSum);
+//                if (!result.contains(threeSum))
+//                    result.add(threeSum);
+//            }
+//        }
+//        return result;
+//    }
+
     public static List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        int[] newNums = new int[nums.length-1];
-        for (int i = 0; i < nums.length; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            for (int j = 0; j < nums.length - 1; j++) {
-                if (j < i)
-                    newNums[j] = nums[j];
-                else
-                    newNums[j] = nums[j + 1];
-            }
-            if (twoSum(newNums, -nums[i]) != null) {
-                List<Integer> threeSum = new ArrayList<>();
-                threeSum.add(nums[i]);
-                for (int n : twoSum(newNums, -nums[i]))
-                    threeSum.add(n);
-                Collections.sort(threeSum);
-                if (!result.contains(threeSum))
-                    result.add(threeSum);
+        int len = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            int pointerL = i + 1;
+            int pointerR = len - 1;
+            if (nums[i] > 0) return result;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            while (pointerL < pointerR) {
+                int sum = nums[i] + nums[pointerL] + nums[pointerR];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i] , nums[pointerL] , nums[pointerR]));
+                    while (pointerL < pointerR && nums[pointerL] == nums[pointerL + 1]) pointerL++;
+                    while (pointerL < pointerR && nums[pointerR] == nums[pointerR - 1]) pointerR--;
+                    pointerL++;
+                    pointerR--;
+                }
+                else if (sum < 0) pointerL++;
+                else pointerR--;
             }
         }
         return result;
     }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         int[] nums = new int[]{-1,0,1,2,-1,-4};
         int target = 18;
         int[] position = twoSum(nums, target);
