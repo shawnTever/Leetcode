e216f25fecf6b3254df8bff0d34934b9c16a0eb1
Shawn <shawntimmel@gmail.com>
2021-02-27 22:12:42 +0800

    _115_offer30_MinStack

--- src/_115_offer30_MinStack.java
+++ src/_115_offer30_MinStack.java
@@ -1,10 +1,29 @@
 import java.util.Stack;
 
 public class _115_offer30_MinStack {
-    Stack<Integer> A, B = new Stack<>();
+    Stack<Integer> A, B;
 
     public _115_offer30_MinStack() {
-
+        A = new Stack<>();
+        B = new Stack<>();
     }
 
+    public void push(int x) {
+        A.push(x);
+        if (B.isEmpty() || B.peek() >= x)
+            B.push(x);
+    }
+
+    public void pop() {
+        if (A.pop().equals(B.peek()))
+            B.pop();
+    }
+
+    public int top() {
+        return A.peek();
+    }
+
+    public int getMin() {
+        return B.peek();
+    }
 }
