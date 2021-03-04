1ea7c13b290bc5bd12e1fda790c68e807f87686c
Shawn <shawntimmel@gmail.com>
2021-03-05 00:52:07 +0800

    _946_offer31_validateStackSequences

--- src/_946_offer31_validateStackSequences.java
+++ src/_946_offer31_validateStackSequences.java
@@ -1,3 +1,17 @@
+import java.util.Stack;
+
 public class _946_offer31_validateStackSequences {
-    
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int i = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int push : pushed) {
+            stack.push(push);
+            while (!stack.isEmpty() && popped[i] == stack.peek()) {
+                stack.pop();
+                i++;
+            }
+        }
+        if (stack.isEmpty()) return true;
+        else return false;
+    }
 }
