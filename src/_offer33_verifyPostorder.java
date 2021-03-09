cb6ae532eee192c5d6fd07cee78cb3ed1ecfea18
Shawn <shawntimmel@gmail.com>
2021-03-10 00:46:45 +0800

    _946_offer31_validateStackSequences

--- src/_offer33_verifyPostorder.java
+++ src/_offer33_verifyPostorder.java
@@ -1,2 +1,22 @@
 public class _offer33_verifyPostorder {
+    public boolean verifyPostorder(int[] postorder) {
+        return recursive(postorder, 0, postorder.length - 1);
+    }
+
+    private boolean recursive(int[] postorder, int i, int j) {
+        if (i >= j) return true;
+        int p = i;
+        while (postorder[p] < postorder[j]) p++;
+        int m = p;
+        while (postorder[p] > postorder[j]) p++;
+        return p == j && recursive(postorder, i, m - 1)
+                && recursive(postorder, m, j - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] postorder = new int[]{1, 2, 5, 10, 6, 9, 4, 3};
+        _offer33_verifyPostorder verifyPostorder = new _offer33_verifyPostorder();
+        System.out.println(verifyPostorder.verifyPostorder(postorder));
+
+    }
 }
