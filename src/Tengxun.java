f3fa047298630964a1016924f4abf44f02f19ba7
Shawn <shawntimmel@gmail.com>
2021-03-22 16:07:14 +0800

    nowcoder Tengxun

---
+++ src/Tengxun.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+public class Tengxun {
+
+//    public ListNode solve (ListNode S) {
+//        // write code here
+//        ListNode root = S;
+//        ListNode max = S;
+//        while (root.next != null) {
+//            root = root.next;
+//            if (root.val > max.val)
+//                max = root;
+//
+//        }
+//        root.next = S;
+//        return max;
+//    }
+
+//    public static int solve (List<String> S) {
+//        // write code here
+//        int n = 0;
+//        int max = 0;
+//        for (String s : S) {
+//            if (s.equals("for")) {
+//                n++;
+//                max = Math.max(max, n);
+//            }
+//            else if (s.equals("end")){
+//                n--;
+//                max = Math.max(max, n);
+//            }
+//        }
+//        return max;
+//    }
+
+
+//    public static int solve (int n, int w, int[] low, int[] high) {
+//        int sumHigh = 0, sumLow = 0;
+//        for (int i : low) sumLow += i;
+//        for (int i : high) sumHigh += i;
+//        if (sumHigh <= w) {
+//            Arrays.sort(high);
+//            return high[n / 2 + 1];
+//        }
+//        else {
+//
+//        }
+//        return high[n / 2 + 1];
+//    }
+
+
+    public static int solve (int n, int m, String[] prices) {
+        int sum = 0;
+        for (String price : prices) {
+            int p = Integer.parseInt(price);
+            sum += p;
+        }
+        int min = sum;
+        for (int num = 1; num <= n; num++) {
+            if (num == 1) {
+                for (String price : prices) {
+                    int p = Integer.parseInt(price);
+                    if (p % m == 0) {
+                        int left = sum - p;
+                        min = Math.min(min, left);
+                    }
+                }
+            }
+
+        }
+        return min;
+    }
+
+
+
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int n = Integer.parseInt(in.nextLine());
+//        List<String> list = new ArrayList<>();
+//        for (int i = 0; i < n; i++) {
+//            String s = in.nextLine();
+//            list.add(s);
+//        }
+//        System.out.println(solve(list));
+
+//        Scanner in = new Scanner(System.in);
+//        String[]  strings = in.nextLine().split(" ");
+//        int n = Integer.parseInt(strings[0]);
+//        int w = Integer.parseInt(strings[1]);
+//        int[] low = new int[n];
+//        int[] high = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            String[]  strings1 = in.nextLine().split(" ");
+//            low[i] = Integer.parseInt(strings1[0]);
+//            high[i] = Integer.parseInt(strings1[1]);
+//        }
+//        System.out.println(solve(n, w, low, high));
+
+
+        Scanner in = new Scanner(System.in);
+        int T = Integer.parseInt(in.nextLine());
+        for (int i = 0; i < T; i++) {
+            String[]  strings = in.nextLine().split(" ");
+            int n = Integer.parseInt(strings[0]);
+            int m = Integer.parseInt(strings[1]);
+            String[]  price = in.nextLine().split(" ");
+            System.out.println(solve (n, m, price));
+        }
+
+    }
+}
---
+++ src/nowcoder.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class nowcoder {
+
+//    public static int solution(int a, int b) {
+//        return a + b;
+//    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+////        sc.nextInt();
+//        while (sc.hasNextInt()) {
+//            int a = sc.nextInt();
+//            int b = sc.nextInt();
+//            if (a == 0 && b == 0) break;
+//            System.out.println(solution(a, b));
+//        }
+//        while (sc.hasNext()) {
+//            int sum = 0;
+//            String s = sc.nextLine();
+//            if (!s.equals("")) {
+//                String[] str = s.split(" ");
+//                if (str[0].equals("0")) break;
+//                for (int i = 1; i < str.length; i++) {
+//                    sum += Integer.parseInt(str[i]);
+//                }
+//                System.out.println(sum);
+//            }
+//        }
+
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            String[] str = s.split(" ");
+            System.out.println(Long.parseLong(str[0]) + Long.parseLong(str[1]));
+        }
+
+    }
+
+}
