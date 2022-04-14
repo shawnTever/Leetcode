aea2b8c63ed962d995f25f50db328227d69c23bf
Shawn <shawntimmel@gmail.com>
2022-04-14 17:57:34 +0800

    全排列字符串 字符串子序列

--- src/_46_permute.java
+++ src/_46_permute.java
@@ -1,8 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class _46_permute {
+//    全排列
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> permute = new ArrayList<>();
         List<Integer> subNums = new ArrayList<>();
@@ -27,6 +26,55 @@ public class _46_permute {
             }
         }
     }
+//    全排列字符串
+    public List<String> permute(String s) {
+        List<String> ans = new ArrayList<>();
+        if (s == null || s.length() == 0) return ans;
+        char[] str = s.toCharArray();
+        ArrayList<Character> rest = new ArrayList<>();
+        for (char ch : str)
+            rest.add(ch);
+        String path = "";
+        dnf(rest, path, ans);
+        return ans;
+    }
+
+    public void dnf(ArrayList<Character> rest, String path, List<String> ans) {
+        if (rest.isEmpty()) ans.add(path);
+        else {
+            int n = rest.size();
+            for (int i = 0; i < n; i++) {
+                char cur = rest.get(i);
+                rest.remove(i);
+                dnf(rest, path + cur, ans);
+                rest.add(i, cur);
+            }
+        }
+    }
+
+//    字符串所有子序列
+    public List<String> permute1(String s) {
+        char[] str = s.toCharArray();
+        String path = "";
+        Set<String> set = new HashSet<>();
+        dnf(str, 0, set, path);
+        return new ArrayList<>(set);
+    }
+
+//    path为之前决定不能改变的序列
+//    str[index]还未决定的序列
+//    Set去重
+    public void dnf(char[] str, int index, Set<String> ans, String path) {
+        if (index == str.length) {
+            ans.add(path);
+            return;
+        }
+//        没要index位置字符
+        dnf(str, index + 1, ans, path);
+//        要index位置字符
+        dnf(str, index + 1, ans, path + String.valueOf(str[index]));
+    }
+
 
     public static void main(String[] args) {
         _46_permute _46_permute = new _46_permute();
--- src/_647_countPalindromicSubstrings.java
+++ src/_647_countPalindromicSubstrings.java
@@ -4,4 +4,17 @@ public class _647_countPalindromicSubstrings {
         return 0;
     }
 
+//    回文子序列计数
+//    暴力列出子序列
+    public int countSubstrings1(String s) {
+        if (s == null || s.length() == 0) return 0;
+        char[] c = s.toCharArray();
+        char[] path = new char[c.length];
+        return 0;
+    }
+
+    private static int process(char[] s, int si, char[] path, int pi) {
+        return 0;
+    }
+
 }
